c97215d8c86495b6ec82103efe0cfe0fe0115c8d
Tiffany Mark <tiffmark@hotmail.com>
2017-02-10 11:52:32 -0200

    cadastro de um novo usuário usando SQLite

--- src/pages/create-account/create-account.ts
+++ src/pages/create-account/create-account.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, MenuController } from 'ionic-angular';
+import { NavController, NavParams, MenuController, Platform} from 'ionic-angular';
+import { SQLite } from 'ionic-native';
+import { Login } from '../login/login';
+
 
 @Component({
   selector: 'page-create-account',
@@ -7,12 +10,50 @@ import { NavController, NavParams, MenuController } from 'ionic-angular';
 })
 export class CreateAccount {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public menuCtrl: MenuController) {
-    this.menuCtrl.enable(false,"logon");
+  public db: SQLite;
+
+  public user = {
+    firstname: '',
+    lastname: '',
+    adress: '',
+    phone: '',
+    birthday: '2017-01-01',
+    email: '',
+    username: '',
+    password: ''
   }
 
-  public birthday = {
-    date: '2017-01-01'
+  constructor(public navCtrl: NavController, public navParams: NavParams, public menuCtrl: MenuController, public platform: Platform) {
+    this.menuCtrl.enable(false,"logon");
+
+    this.platform.ready().then(() => {
+    this.db = new SQLite();
+      this.db.openDatabase({
+          name: "CarRental.db",
+          location: "default"
+      }).then(() => {
+          this.db.executeSql("CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY AUTOINCREMENT, firstname TEXT, lastname TEXT, adress TEXT, phone TEXT, birthday TEXT, email TEXT, username TEXT, password TEXT)", {}).then((data) => {
+              console.log("TABLE CREATED: ", data);
+          }, (error) => {
+              console.error("Unable to execute sql", error);
+          })
+        }, (error) => {
+          console.error("Unable to open database", error);
+        });
+    });
+
+  }
+
+  newAccount(){
+    this.db.executeSql("INSERT INTO user (firstname, lastname, adress, phone, birthday, email, username, password) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", [this.user.firstname,this.user.lastname,this.user.adress,this.user.phone,this.user.birthday,this.user.email,this.user.username,this.user.password]).then((data) => {
+            console.log(this.user.firstname,this.user.lastname,this.user.adress,this.user.phone,this.user.birthday,this.user.email,this.user.username,this.user.password);
+
+            console.log("INSERTED: " + JSON.stringify(data));
+        }, (error) => {
+            console.log("ERROR: " + JSON.stringify(error.err));
+        });
+    this.navCtrl.push(Login);
+
   }
   
 
