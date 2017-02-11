ffe8b7d4100bf4fbd88631c0446be5798ecabf9c
Tiffany Mark <tiffmark@hotmail.com>
2017-02-11 01:19:20 -0200

    botão de envio nos formulários
    ficará inativo até todos os campos serem preenchidos

--- src/pages/create-account/create-account.ts
+++ src/pages/create-account/create-account.ts
@@ -44,6 +44,11 @@ export class CreateAccount {
 
   }
 
+  invalidCreateAccountForm(){
+    if(this.user.firstname.length == 0 || this.user.lastname.length == 0 || this.user.adress.length == 0 || this.user.phone.length == 0 || this.user.birthday.length == 0 || this.user.email.length == 0 || this.user.username.length == 0 || this.user.password.length == 0) return true;
+            return false;
+    }
+
   newAccount(){
     this.db.executeSql("INSERT INTO user (firstname, lastname, adress, phone, birthday, email, username, password) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", [this.user.firstname,this.user.lastname,this.user.adress,this.user.phone,this.user.birthday,this.user.email,this.user.username,this.user.password]).then((data) => {
             console.log(this.user.firstname,this.user.lastname,this.user.adress,this.user.phone,this.user.birthday,this.user.email,this.user.username,this.user.password);
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -23,6 +23,8 @@ export class Login {
     password: ''
   }
 
+  userFirstname : string;
+
   constructor(public navCtrl: NavController, public navParams: NavParams, public menuCtrl: MenuController, public platform: Platform, public alertCtrl: AlertController) {
     this.menuCtrl.enable(false,"logon");
 
@@ -38,21 +40,27 @@ export class Login {
 
   }
 
+  invalidLoginForm(){
+    if (this.user.username.length == 0 || this.user.password.length == 0) return true ;
+                     return false ;
+  }
+
   login(){
     this.db.executeSql("SELECT * FROM user WHERE username = ? AND password = ?", [this.user.username, this.user.password]).then((data) => {
       console.log("LENGTH: ",data.rows.length);
       if(data.rows.length == 1){
         console.log("FOUND");
+        console.log("NAME: ",data.rows.item(0).firstname);
         this.menuCtrl.enable(true,"logon");
         this.navCtrl.setRoot(Home);
       }
       else{
-        let alert = this.alertCtrl.create({
+        let alertNotFound = this.alertCtrl.create({
           title: 'User not found',
           subTitle: 'Your username or password is incorrect. Please try again.',
           buttons: ['OK']
         });
-        alert.present();
+        alertNotFound.present();
       }
     }, (error) => {
         console.error("Unable to execute sql", error);
